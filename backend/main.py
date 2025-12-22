3030a986e14624d4feb0581f44a77670411523cc
KaustubhKasture <kaustubh.kasture10@gmail.com>
2025-12-23 00:46:18 +0530

    CORS bug fix

--- backend/main.py
+++ backend/main.py
@@ -1,21 +1,15 @@
-import os
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from api.routes import router as api_router
 
 app = FastAPI()
 
-origins = ["http://localhost:3000"]
-if os.getenv("VERCEL_URL"):
-    origins.append(f"https://{os.getenv('VERCEL_URL')}")
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["GET", "POST", "OPTIONS"],
+    allow_origins=["*"],
+    allow_credentials=False,
+    allow_methods=["*"],
     allow_headers=["*"],
-    max_age=86400,
 )
 
 app.include_router(api_router,prefix="/api")
